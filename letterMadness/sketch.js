501c6bcbd207e2b4151f0beafa535de200c51f56
Jade <jingfeilin@me.com>
2017-05-10 02:27:03 -0400

    moving up and falling down

--- letterMadness/sketch.js
+++ letterMadness/sketch.js
@@ -1,9 +1,11 @@
 var alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
-var cols=280;
-var rows=190;
+var cols=100;
+var rows=70;
 var horizontalTextSize;
 var verticalTextSize;
 var letters=[];
+var isMoving=false;
+var isFalling=false;
 function setup() {
   createCanvas(displayWidth, displayHeight);
   background(0);
@@ -21,20 +23,50 @@ function setup() {
       letters[i][j].display();
     }
   }
-}
 
-function draw() {
-
-  horizontallyMove(20);
-  horizontallyMove(12);
-  horizontallyMove(50);
-  horizontallyMove(51);
+    console.log(letters.length);
 }
 
 function mousePressed(){
-  // horizontallyMove(parseInt(random(rows)));
-  horizontallyMove(20);
+  isMoving=true;
 }
+function keyPressed(){
+
+    isFalling=true;
+    console.log(isFalling);
+
+}
+
+function draw() {
+  background(0);
+
+  if(isMoving){
+    for (var i=0;i<cols;i++){
+      for(var j=0;j<rows;j++){
+        letters[i][j].moveUp(20);
+      }
+    }
+  }
+
+  if(isFalling){
+    for (var i=0;i<cols;i++){
+      for(var j=0;j<rows;j++){
+        letters[i][j].fall();
+      }
+    }
+  }
+
+  for (var i=0;i<cols;i++){
+    for(var j=0;j<rows;j++){
+      letters[i][j].display();
+    }
+  }
+  // horizontallyMove(20);
+  // horizontallyMove(12);
+  // horizontallyMove(50);
+  // horizontallyMove(51);
+}
+
 
 function horizontallyMove(n){
   var head=letters[0][n].letter;
@@ -55,9 +87,90 @@ function Letter(x,y){
   this.letter=alphabets[parseInt(random(alphabets.length))];
   this.x=x*horizontalTextSize;
   this.y=y*verticalTextSize;
+
+  this.wiggle=true;
+  this.velocityX=0;
+  this.velocityY=0;
+
+
+  this.fall=function(){
+    if(abs(this.y-50)<5){
+      this.velocityY=random(240,280);
+    }
+    else if(abs(this.y-50)<10  && abs(this.y-50)>5){
+      this.velocityY=random(200,240);
+    }
+    else if(abs(this.y-50)<15  && abs(this.y-50)>10){
+      this.velocityY=random(160,200);
+    }
+    else if(abs(this.y-50)<25  && abs(this.y-50)>15){
+      this.velocityY=random(120,160);
+    }
+    else{
+      this.velocityY=random(90,120);
+    }
+
+    this.velocityX=0;
+
+    if(abs(this.y-height)<30){
+      this.velocityY=random(-10,10);
+    }
+
+    this.y+=this.velocityY;
+  }
+
+  this.moveUp=function(targetY){
+
+    if(abs(targetY-this.y)>50){
+      this.wiggle=false;
+      this.velocityY=random(8,20);
+      this.velocityX=random(-5,5);
+    }
+    else{
+      this.wiggle=true;
+    }
+    if(this.wiggle===true){
+    this.velocityX=random(-2,2);
+    this.velocityY=random(-20,20);;
+    }
+
+      this.y-=this.velocityY;
+      this.x+=this.velocityX;
+
+
+    //   if(abs(this.y-targetY)>abs(this.x-targetX)){
+    //     this.velocityY=map(abs(this.y-targetY),0,height,1,14)*random(1,2);
+    //     this.velocityX=map(abs(this.x-targetX),0,width,1,15)*random(0.5,1);
+    //   }
+    //   else{
+    //     this.velocityY=map(abs(this.y-targetY),0,height,1,14)*random(0.5,1);
+    //     this.velocityX=map(abs(this.x-targetX),0,width,1,15)*random(1,2);
+    //   }
+    //
+    //   if(this.x<targetX){
+    //     this.x+=this.velocityX;
+    //   }
+    //   else if(this.x>=targetX){
+    //     this.x-=this.velocityX;
+    //   }
+    //   if(this.y<targetY){
+    //     this.y+=this.velocityY;
+    //   }
+    //   else if(this.y>=targetY){
+    //     this.y-=this.velocityY;
+    //   }
+      // else if(this.y>targetY){
+      //   this.y-=this.velocityY;
+      // }
+    // }
+    // else{
+    //   this.pause=true;
+    // }
+  }
+
   this.display=function(){
     fill(0, 255, 255);
-    textSize(3);
+    // textSize(3);
     text(this.letter, this.x, this.y);
   }
 }
